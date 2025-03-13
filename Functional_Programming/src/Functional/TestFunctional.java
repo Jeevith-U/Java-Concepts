9e0380b7e4747d96b2967e7d37646a67b8e760fd
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2025-03-13 15:25:37 +0530

    Started With Functional Programming

---
+++ Functional_Programming/src/Functional/TestFunctional.java
@@ -0,0 +1,17 @@
+package Functional;
+
+import java.util.function.Function;
+
+public class TestFunctional {
+	
+	public static void main(String[] args) {
+		
+		/**
+		 * Function<Integer, Integer>
+		 * 			input	 output
+		 */
+		Function<Integer, Integer> fun = i -> i * i ;
+		
+		System.out.println("The Square of 10 is : "+ fun.apply(10));
+	}
+}
